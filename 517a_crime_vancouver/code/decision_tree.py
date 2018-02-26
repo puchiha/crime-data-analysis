4457c67ef4b7673250fdf59baa16e95e0555cb4c
Prakrit <prakrit@github.com>
2018-02-25 23:24:48 -0600

    1

--- 517a_crime_vancouver/code/decision_tree.py
+++ 517a_crime_vancouver/code/decision_tree.py
@@ -1,5 +1,6 @@
 from sklearn import tree
-from sklearn.tree import DecisionTreeClassifier, export_graphviz
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.cross_validation import  cross_val_score
 from common import *
 
 def get_crime_data():
@@ -11,29 +12,23 @@ def get_crime_data():
 		
 	return df
 
-def visualize_tree(tree, feature_names):
-    with open("dt.dot", 'w') as f:
-        export_graphviz(tree, out_file=f,
-                        feature_names=feature_names)
-
-    command = ["dot", "-Tpng", "dt.dot", "-o", "dt.png"]
-    try:
-        subprocess.check_call(command)
-    except:
-        exit("Could not run dot, ie graphviz, to "
-             "produce visualization")
-
 def d_tree():
 	df = get_crime_data()
 	features = list(df.columns[:9])
 	y = df["CLASSIFICATION"]
 	X = df[features]
-
-	dt = DecisionTreeClassifier(min_samples_split=20, random_state=99)
-
-	w = dt.fit(X, y)
-	visualize_tree(dt, features)
-	print w
+	#	x_train,x_test,y_train,y_test = cross_validation.train_test_split(X,y,test_size=0.4,random_state=0)
+	
+	dt = DecisionTreeClassifier(min_samples_split = 20, random_state = 99)
+	print("-- 10-fold cross-validation --")
+	cv_dt = cross_val_score(dt, X, y, cv=10)
+	fit_dt = dt.fit(X, y)
+	print("mean: {:.3f} (std: {:.3f})".format(cv_dt.mean(),
+                                          cv_dt.std()))
+                                         
+	
+	#visualize_tree(dt, features)
+	
 
 if __name__ == "__main__":
     d_tree() 
