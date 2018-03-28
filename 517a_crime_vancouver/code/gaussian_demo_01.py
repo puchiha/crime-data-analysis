688d1936bcb48cafcb48b26e94a2192ec6bb3d1b
Prakrit <prakrit@github.com>
2018-03-27 23:43:10 -0500

    initial_commit

---
+++ 517a_crime_vancouver/code/gaussian_demo_01.py
@@ -0,0 +1,13 @@
+ '''
+ Train and run Gaussian Processes. Evaluate and compare the predictions using at least two differnt kernels via 10-fold cross-validation with a suitable error measure (we recommend negative log predictiv density as it takes the predictive uncertainty into account).
+
+ Prakrit Shrestha | 2018
+ Crime Data Analysis - Running a basic GP
+
+
+ '''
+
+
+
+
+
